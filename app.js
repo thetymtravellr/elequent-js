66de8d6ef8c9c076991c56f2bfde3513434ec94a
Robiul Hasan <muhammadrobiul502@gmail.com>
2021-11-03 07:37:54 +0600

    elequent js

---
+++ app.js
@@ -0,0 +1,37 @@
+'use strict';
+
+// ARITHMETIC
+
+console.log( 100 + 4 ); //addition
+console.log( 100 - 4 ); //subtraction
+console.log( 100 * 4 ); //multiplication
+console.log( 100 / 4 ); //division
+console.log( 100 ** 4 ); //exponent
+
+
+//STRINGS
+
+console.log('Down on the sea');
+console.log("Lie on the ocean");
+console.log("Aren't you there");
+console.log(`"Hello World"`);
+
+console.log("con" + "cat" + "e" + "nate");
+console.log(`concatenate`);
+console.log(`half of 100 is ${100 / 2}`);
+
+// UNARY OPERATORS
+
+console.log(typeof 4.5);
+console.log(typeof 'Hello');
+console.log(typeof true);
+
+//BOOLEAN VALUES
+
+const isMale = true;
+const isAdult = false;
+
+// COMPARISON
+
+console.log(3 > 2); // > stand for is greater than
+console.log(3 < 2); // < stand for is less than
